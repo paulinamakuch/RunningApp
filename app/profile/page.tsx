fc5afd2dd6a8f8b2842b11361d78f3204031f5d9
Kevin Patrick Westropp <highlanderkev@proton.me>
2024-03-03 21:22:38 -0700

    Changes: - Add some scripts to the package.json for working with SST and next - Create new dir and page.tsx for route /profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+export default function Profile() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="/profile">Profile</Link>
+          <Link href="/">Home</Link>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -23,14 +23,13 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          loader={imageLoader}
-          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Frunning&psig=AOvVaw3DlO47bziW-00JmVE8QbbL&ust=1709611695439000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCPDg5f_d2YQDFQAAAAAdAAAAABAE"
-          alt="Running Image"
+          src="/next.svg"
+          alt="Next.js Logo"
           width={180}
           height={37}
           priority
         /> */}
-        <h1>Running App - Home</h1>
+        <h1>Running App - Profile</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -103,5 +102,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
+    )
 }
